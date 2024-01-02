bd8322dd733a44ed5ca0ec28d63a1d29076883d8
koinoniays2 <koinonia_ys@naver.com>
2024-01-02 09:21:00 +0900

    event

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-// import EventPractice from "./EventPractice";
-// import EventPractice2 from "./EventPractice2";
-
 import EventQuiz from "./EventQuiz";
 
 export default function App() {
   return (
     <div className="App">
-      {/* <EventPractice2 /> */}
       <EventQuiz />
     </div>
   );
--- src/EventQuiz.jsx
+++ src/EventQuiz.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react"
 
 export default function EventQuiz() {
-    const [form, setForm] = useState({
+    const [form, setForm] = useState({ //form에 object초기화
         username:'',
         password:'',
         email:'',
         phone:''
     });
+    // 화면 출력
     const [output, setOutput] = useState({
         username:'',
         password:'',
         email:'',
         phone:''
       });
-
+    
     const {username, password, email, phone} = form;
 
     const onChange = e => {
@@ -25,13 +26,25 @@ export default function EventQuiz() {
     }
     
     const onClick = () => {
-        setOutput({ ...form });
-        setForm({
-            username:'',
-            password:'',
-            email:'',
-            phone:''
-        })
+        if (Object.values(form).every(value => value !== '')) {
+            setOutput({ ...form });
+            setForm({
+              username: '',
+              password: '',
+              email: '',
+              phone: ''
+            });
+          } else {
+            // 한 칸이라도 비어있을 경우 알림창
+            alert('모든 필드를 입력하세요');
+            setForm({
+                username: '',
+                password: '',
+                email: '',
+                phone: ''
+            });
+            setOutput({ ...form });
+        }
     };
     const onKeyPress = e => {
         if(e.key === 'Enter') {
@@ -48,12 +61,14 @@ export default function EventQuiz() {
             <input type="text" name="email" placeholder="이메일" onChange={onChange} value={email}/>
             <input type="text" name="phone" placeholder="휴대폰번호" onChange={onChange} value={phone} onKeyDown={onKeyPress}/>
             <button className="w-36 h-8 rounded-xl bg-purple-200" onClick={onClick}>확인</button>
+            {Object.values(output).every(value => value !== '') && (
             <div>
-                <p>이름: {output.username}</p>
-                <p>비밀번호: {output.password}</p>
-                <p>이메일: {output.email}</p>
-                <p>휴대폰번호: {output.phone}</p>
+            <p>이름: {output.username}</p>
+            <p>비밀번호: {output.password}</p>
+            <p>이메일: {output.email}</p>
+            <p>휴대폰번호: {output.phone}</p>
             </div>
+        )}
         </div>
     </>
   )
